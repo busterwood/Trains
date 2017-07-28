a62574ba2683b6c9c79b851785f61371b804ae7e
BusterWood <chris.j.austin@gmail.com>
2017-07-28 17:04:47 +0100

    initial commit

---
+++ BusterWood.Trains/Class1.cs
@@ -0,0 +1,178 @@
+﻿using BusterWood.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusterWood.Trains
+{
+    interface IReserveTrains
+    {
+        ReservationResult Reserve(ReservationRequest attempt);
+    }
+
+    struct ReservationRequest
+    {
+        public TrainId TrainId { get; }
+        public string BookingRef { get; }
+        public IReadOnlySet<SeatId> Seats { get; }
+
+        public ReservationRequest(TrainId trainId, string bookingRef, UniqueList<SeatId> seats)
+        {
+            TrainId = trainId;
+            BookingRef = bookingRef;
+            Seats = seats;
+        }
+    }
+
+    interface IFindTheBestSeats
+    {
+        ReservationResult Reserve(SeatRequest seats);
+    }
+
+    struct SeatRequest
+    {
+        public TrainId TrainId { get; }
+        public string BookingRef { get; }
+        public int NumberOfSeats { get; }
+
+        public SeatRequest(TrainId trainId, string bookingRef, int seats)
+        {
+            TrainId = trainId;
+            BookingRef = bookingRef;
+            NumberOfSeats = seats;
+        }
+
+        public override string ToString() => $"Booking: '{BookingRef}', Train: '{TrainId}', Seats: {NumberOfSeats}";
+    }
+    
+    /// <remarks>Struc because not nullable</remarks>
+    struct ReservationResult
+    {
+        public TrainId TrainId { get; }
+        public string BookingRef { get; }
+        public UniqueList<SeatId> Seats { get; }
+
+        public ReservationResult(TrainId trainId, string bookingRef, UniqueList<SeatId> seats)
+        {
+            TrainId = trainId;
+            BookingRef = bookingRef;
+            Seats = seats;
+        }
+
+        public bool Successful => !string.IsNullOrEmpty(BookingRef);
+    }
+
+    interface IFindTrains
+    {
+        UniqueList<TrainId> FindTrains(DateTime date);
+        TrainTopology GetTopology(TrainId trainId);
+    }
+
+    class TrainTopology
+    {
+        public TrainId TrainId { get; }
+        public UniqueList<Seat> Seats { get; }
+    }
+
+    struct Seat
+    {
+        public SeatId SeatId { get; }
+        public string BookingRef { get; }
+
+        public bool IsFree => string.IsNullOrEmpty(BookingRef);
+    }
+
+    struct SeatId
+    {
+        public string Number { get; }
+        public string Coach { get; }
+
+        public SeatId(string id)
+        {
+            if (id == null)
+                throw new ArgumentNullException(nameof(id));
+            if (id.Length < 2)
+                throw new ArgumentException(nameof(id));
+            var len = id.Length;
+            Number = Coach = id.Substring(0, len - 1);
+            Coach = id.Substring(len - 1);
+        }
+
+        public override string ToString() => $"{Number}{Coach}";
+    }
+
+    struct TrainId
+    {
+        readonly string id;
+
+        public TrainId(string id)
+        {
+            this.id = id;
+        }
+
+        public override string ToString() => id;
+    }
+
+
+    class FindTheBestSeats : IFindTheBestSeats
+    {
+        readonly IFindTrains trainFinder;
+        readonly IReserveTrains reservations;
+
+        public FindTheBestSeats(IFindTrains findTrains, IReserveTrains reservations)
+        {
+            this.trainFinder = findTrains;
+            this.reservations = reservations;
+        }
+
+        public ReservationResult Reserve(SeatRequest request)
+        {
+            var train = trainFinder.GetTopology(request.TrainId);
+            if (!train.Seats.ReservationAllowed())
+                return ReservationFailed(request.TrainId);
+
+            var coaches = GroupByCoach(train);
+
+            foreach (var possibility in PossibleReservations(coaches, request.NumberOfSeats))
+            {
+                var result = reservations.Reserve(new ReservationRequest(request.TrainId, request.BookingRef, possibility));
+                if (result.Successful)
+                    return result;
+            }
+
+            return ReservationFailed(request.TrainId);
+        }
+
+        static ReservationResult ReservationFailed(TrainId trainId) => new ReservationResult(trainId, "", new UniqueList<SeatId>());
+
+        static IEnumerable<UniqueList<Seat>> GroupByCoach(TrainTopology train)
+        {
+            return train.Seats
+                .GroupBy(s => s.SeatId.Coach)
+                .Select(grp => grp.OrderBy(s => s.SeatId.Number).ToUniqueList());
+        }
+
+        static IEnumerable<UniqueList<SeatId>> PossibleReservations(IEnumerable<UniqueList<Seat>> coaches, int numberOfSeats)
+        {            
+            return coaches
+                .Where(coach => coach.ReservationAllowed()) // coach has enough free seats
+                .Select(coach => coach.FindFreeSeats(numberOfSeats)) // find seats, add booking ref
+                .Where(seats => seats.Count == numberOfSeats); // where the whole party will fit on the coach
+        }
+
+    }
+
+    static class BusinessExtensions
+    {
+        public static bool ReservationAllowed(this IReadOnlySet<Seat> seats)
+        {
+            var emptySeats = seats.Where(s => string.IsNullOrEmpty(s.BookingRef)).Count();
+            return ((emptySeats * 100) / seats.Count) < 70;
+        }
+
+        public static UniqueList<SeatId> FindFreeSeats(this IReadOnlySet<Seat> seats, int number)
+        {
+            return seats.Where(s => s.IsFree).Take(number).Select(s => s.SeatId).ToUniqueList(); // todo: adjasent seats
+        }
+    }
+}
---
+++ BusterWood.Trains/Class2.cs
@@ -0,0 +1,119 @@
+﻿using BusterWood.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusterWood.Trains
+{
+    /// <summary>
+    /// Pure business logic for attempting reservation of seats for a party of N people.
+    /// </summary>
+    /// <remarks>
+    /// Note that this class does no logging and has no dependencies, see <see cref="ActualBestSeatFinder"/> for concreate implementation.
+    /// Methods are virtual so subclass can add logging.
+    /// </remarks>
+    abstract class BestSeatFinder
+    {
+        public ReservationResult Reserve(SeatRequest request)
+        {
+            TrainTopology train = GetTrainTopology(request);
+
+            if (TrainWillBeFull(train, request))
+                return ReservationFailed(request);
+
+            foreach (var coach in CoachesWithEnoughFreeSeats(train, request))
+            {
+                var result = AttemptReservation(coach, request);
+                if (result.Successful)
+                    return result;
+            }
+            return ReservationFailed(request);
+        }
+
+        protected abstract TrainTopology GetTrainTopology(SeatRequest request);
+
+        protected virtual ReservationResult ReservationFailed(SeatRequest request) => new ReservationResult(request.TrainId, "", new UniqueList<SeatId>());
+
+        protected virtual bool TrainWillBeFull(TrainTopology train, SeatRequest request) => IsFull(train.Seats, request.NumberOfSeats);
+
+        protected virtual bool IsFull(UniqueList<Seat> seats, int requestedNumberOfSeats = 0)
+        {
+            if (seats.Count == 0) return true;
+            float freeSeats = seats.Count(s => s.IsFree) - requestedNumberOfSeats;
+            return freeSeats / seats.Count > 0.7f;
+        }
+
+        protected virtual IEnumerable<UniqueList<SeatId>> CoachesWithEnoughFreeSeats(TrainTopology train, SeatRequest request)
+        {
+            var coaches = train.Seats
+                .GroupBy(s => s.SeatId.Coach)
+                .Select(grp => grp.OrderBy(s => s.SeatId.Number).ToUniqueList());
+
+            return coaches
+                .Where(coach => !IsFull(coach))                                 // coach has enough free seats
+                .Select(coach => FindFreeSeats(coach, request.NumberOfSeats))   // find the free ones
+                .Where(seats => seats.Count == request.NumberOfSeats);          // where the whole party will fit on the coach
+        }
+
+        protected virtual UniqueList<SeatId> FindFreeSeats(UniqueList<Seat> coach, int numberOfSeats)
+        {
+            return coach
+                .Where(s => s.IsFree)
+                .Take(numberOfSeats)
+                .Select(s => s.SeatId)
+                .ToUniqueList(); 
+        }
+
+        protected abstract ReservationResult AttemptReservation(UniqueList<SeatId> seats, SeatRequest request);
+    }
+
+    /// <summary>
+    /// Implements <see cref="BestSeatFinder"/> with dependencies and adds logging
+    /// </summary>
+    /// <remarks>
+    /// Overrides abstract methods to implment service calls. Override virtual methods to add logging.
+    /// </remarks>
+    class ActualBestSeatFinder : BestSeatFinder
+    {
+        readonly IFindTrains trainFinder;
+        readonly IReserveTrains reservations;
+
+        public ActualBestSeatFinder(IFindTrains trainFinder, IReserveTrains reservations)
+        {
+            this.trainFinder = trainFinder;
+            this.reservations = reservations;
+        }
+
+        protected override TrainTopology GetTrainTopology(SeatRequest request) => trainFinder.GetTopology(request.TrainId);
+
+        protected override bool TrainWillBeFull(TrainTopology train, SeatRequest request)
+        {
+            var full = base.TrainWillBeFull(train, request);
+            var msg = full ? "full" : "not full";
+            Console.Error.WriteLine($"INFO: {request}, train is {msg}");
+            return full;
+        }
+
+        protected override IEnumerable<UniqueList<SeatId>> CoachesWithEnoughFreeSeats(TrainTopology train, SeatRequest request)
+        {
+            var coaches = base.CoachesWithEnoughFreeSeats(train, request);
+            Console.Error.WriteLine($"INFO: {request}, there are {coaches.Count()} coaches with enough free space");
+            return coaches;
+        }
+
+        protected override ReservationResult ReservationFailed(SeatRequest request)
+        {
+            Console.Error.WriteLine($"INFO: {request}, request failed");
+            return base.ReservationFailed(request);
+        }
+
+        protected override ReservationResult AttemptReservation(UniqueList<SeatId> seats, SeatRequest request)
+        {
+            var req = new ReservationRequest(request.TrainId, request.BookingRef, seats);
+            var result = reservations.Reserve(req);
+            if (result.Successful)
+                Console.Error.WriteLine($"INFO: {request}, request succeeded");
+            return result;
+        }
+    }
+}
